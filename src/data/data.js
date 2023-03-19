435938f1c288b1e3d857558cf43b83a78a492ec4
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-19 22:49:47 +0100

    project set up

---
+++ src/data/data.js
@@ -0,0 +1,59 @@
+const data = [
+	{
+		title: "How I got into marketing and why I love it",
+		subTitle:
+			"I get asked too often than not why I'm a marketer, and there's a good reason for it.",
+		dateTime: "6/9/2021",
+		duration: "1 min read",
+		body: `I've always adored well-executed adverts and marketing campaigns. 
+                I was the odd kid who could sing along to most TV and radio ad jingles, and would take my time 
+                dissecting a good full-page magazine ad. With that in mind, it's really not a surprise that I 
+                would find myself drifting towards the world of digital marketing when it came to settling on a career. 
+                Among some of my first online marketing jobs were ad hoc style paid search engine and social media ad 
+                management positions that paid very little but helped me gain a deep understanding of the 
+                field as a whole.`,
+		image:
+			"https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1016,h=432,fit=crop/navyhgblx/convertkit-unsplash-Yane7J3yK3uXZLVz.jpg",
+	},
+	{
+		id: "2",
+		title: "The 10 waysid: '1', you're ruining your content",
+		subTitle:
+			"Your content marketing strategy lives in a volatile environment.",
+		dateTime: "6/9/2021",
+		duration: "1 min read",
+		body: `Content seems to be everywhere nowadays. Every other company is launching a podcast, 
+        trying to beat each other with witty cat memes on social media and providing not-so-gentle sales-heavy 
+        insights into their products in their blogs. While I still stand by the fact that content is king, 
+        there are certain tendencies that inexperienced marketers love that are actually killing your content strategy. 
+        Your content should be, by definition, something engaging and interesting and in particular, 
+        tailored to your customer's buying journey. 
+        The best content is given to the consumer just when they need it.`,
+		image:
+			"https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1016,h=432,fit=crop/navyhgblx/convertkit-unsplash-Yane7J3yK3uXZLVz.jpg",
+	},
+	{
+		id: "3",
+		title: "Rank higher on Google with these 13 tips",
+		subTitle:
+			"SEO can seem like a minefield, and Google's changing algorithm doesn't always help, either.",
+		dateTime: "6/9/2021",
+		duration: "1 min read",
+		body: `Many people ask me to give them a secret fast-tracking recipe for SEO success. Ranking high on Google in a bigger market can be the making or breaking of a newly established online venture, but there simply are no overnight cures to suddenly lift your pages to the very top. Unless you know some really powerful people at Google, I guess. But there are plenty of ways to boost your SEO and they are proven to work, if you’re willing to put in the work. This means that you need to be treating your website as a living and breathing thing, something organic and dynamic, rather than a static thing you can forget about once it’s launched.`,
+		image:
+			"https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1016,h=432,fit=crop/navyhgblx/convertkit-unsplash-mv0MLJaOv0uD2Bkx.jpg",
+	},
+	{
+		id: "4",
+		title: "Spice up your email marketing game in 8 steps",
+		subTitle:
+			"The emails you send out shouldn't be annoying. Learn how to ace your email marketing.",
+		dateTime: "6/9/2021",
+		duration: "1 min read",
+		body: `Put your hand up if you've received a really annoying sales-y email this week that you flagged up as spam since you didn’t really care for the company, or had received the same offer three times already. Emails can be a true gold mine to marketers, yielding an amazing return on investment, but can also quickly result in spam filter triggers and just an overall bad reputation among the email servers, when done wrong. Learning how to create and run successful and engaging email marketing campaigns is a sought-after skill that can transform the way you’re doing business.`,
+		image:
+			"https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1016,h=432,fit=crop/navyhgblx/soundtrap-C-unsplash-m5KOPnj6xlCrkN0l.jpg",
+	},
+];
+
+export default data;
