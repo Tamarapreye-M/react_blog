bf74e78bca42844b6c0c69b4d60ae60d7f8e42c9
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-22 23:35:26 +0100

    useOutletContext Hook added

--- src/pages/Blog/SinglePost.jsx
+++ src/pages/Blog/SinglePost.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import data from "../../data/data";
+import { useParams, useOutletContext } from "react-router-dom";
 
 const SinglePost = () => {
 	const { id } = useParams();
+	const [allPost] = useOutletContext();
+	const post = allPost.find((each) => each.id === id);
 	return (
-		<div>
-			<h1 className=" text-blue-600">Hello, I am star boy, wizkid's mentor</h1>
-			<p>{id}</p>
+		<div className="w-2/3 mx-auto flex flex-col gap-8">
+			<div>
+				<h1 className="text-3xl font-bold">{post.title}</h1>
+				<p className="py-2">{post.subTitle}</p>
+				<p className="flex gap-1">
+					<span>{post.dateTime}</span>
+					<span>.</span>
+					<span>{post.duration}</span>
+				</p>
+			</div>
+			<div>
+				<img src={post.image} alt="" />
+			</div>
+			<p className="w-5/6 mx-auto pt-4">{post.body}</p>
 		</div>
 	);
 };
