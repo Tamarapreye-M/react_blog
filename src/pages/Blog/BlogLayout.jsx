18b10778a10e4780707981c74446e2dbebdbfea0
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-22 23:34:34 +0100

    context added

--- src/pages/Blog/BlogLayout.jsx
+++ src/pages/Blog/BlogLayout.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../../data/data";
 import { Outlet } from "react-router-dom";
 
 const BlogLayout = () => {
+	const [allPost, setAllPost] = useState(data);
+	const handlePost = (ev, id) => {
+		ev.preventDefault();
+		let { name, value } = ev.target;
+		console.log(name, value);
+
+		// const newPost = { [name]: value, id: allPost.length + 1 };
+
+		// setAllPost((prev) => [...prev, newPost]);
+	};
+
 	return (
 		<div>
-			<Outlet />
+			<Outlet context={[allPost, setAllPost, handlePost]} />
 		</div>
 	);
 };
