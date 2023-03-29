737afa29c30b3316e9b4c5062dbf7b6ab6bbe429
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-29 19:19:14 +0100

    styles updated

--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Episodes = () => {
-	return <div className="w-full">Episodes</div>;
+	return <div className="w-full max-w-full">Episodes</div>;
 };
 
 export default Episodes;
