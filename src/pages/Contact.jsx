5ae16fe2b9fd1b4e0ffd7a4c773b634b747e1246
Tamarapreye-M <preyemichael116@gmail.com>
2023-04-13 23:40:33 +0100

    slight changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-	return <div className="w-full ">Contact</div>;
+	return <div className="w-full max-w-full">Contact</div>;
 };
 
 export default Contact;
